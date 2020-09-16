e27e0e3c9653f207f04a778d29156ddc51f73632
mraees93 <53144654+mraees93@users.noreply.github.com>
2020-09-16 12:16:09 +0200

    changed package.json

---
+++ client/build/precache-manifest.c9adb7623158852425ad1d0d4dfec2aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8d9cbfe6d68f3a5656638dcf8ecd3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51a7cc392e4dd3b16f50",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7c6959fafc21c6e0c7aa",
+    "url": "/static/css/main.d283636b.chunk.css"
+  },
+  {
+    "revision": "51a7cc392e4dd3b16f50",
+    "url": "/static/js/2.92b4e91b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.92b4e91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6959fafc21c6e0c7aa",
+    "url": "/static/js/main.010d86cd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
